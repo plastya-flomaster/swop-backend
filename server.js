a73aa4bbae4967a19303b0c31becd5b9a3d8822c
Plastya <nastyapl98@gmail.com>
2020-04-11 14:31:00 +0500

    adding routes to main file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 const app = express();
 
-//bodyparser
+//bodyparser midware
 app.use(
     bodyParser.urlencoded({
         extended: false
@@ -22,6 +25,15 @@ mongoose.connect(db, {
 })
 .then(() => {console.log('connected!')})
 .catch((err) => console.log(err));
+
+//passport midware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
+
+//routes 
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {console.log(`server is running on port ${port}`)})
