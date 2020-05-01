d2842c7357aa2369c6937b8db73b0b1ac3c03975
Dmitriy Putilov <dmitrii.98.putilov@gmail.com>
2020-05-01 13:46:44 +0500

    add comments

--- routes/api/likedItems.js
+++ routes/api/likedItems.js
@@ -3,10 +3,18 @@ const router = express.Router();
 
 const likedItems = require('../../controllers/likedItems.controller');
 
+//Создается объект для likeItems
 router.post('/create/:id', likedItems.create);
 
+//Добавление понравившейся вещи
+//Принимает
+//id = id пользователся,
+//otherId = id владельца понравившейся вещи,
+//itemId = id понравившейся вещи
 router.put('/addpairs', likedItems.addPairs);
 
+//Поиск совподений
+// Принимает userId = id пользователя
 router.get('/search/:id', likedItems.search);
 
 module.exports = router;
